492070966e69199ef814134aea27059a805ae113
Peter Spasov <peters152@icloud.com>
2024-09-09 10:18:14 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,11 +2,6 @@
 apt update
 apt-get install apt-transport-https ca-certificates curl software-properties-common
 
-#Adding docker repo
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
-apt update
-
 #installing Docker and nginx
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository \
